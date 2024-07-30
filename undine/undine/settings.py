7604e61972d37f5a90c1167ed9869824c8ad15ca
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2024-07-30 17:11:07 +0300

    adding render host

--- undine/undine/settings.py
+++ undine/undine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j(u=jm3(%$u7n)86lwr4!pqv@s)c54x4=@404_6-)vzm(z%b8w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'undine.onrender.com']
 
 
 # Application definition
